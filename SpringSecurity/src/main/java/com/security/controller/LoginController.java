d100ca819cb722eb1bac49a53044985b0e9e2c21
unknown <harryw123@163.com>
2019-05-22 21:12:57 +0800

    first commit

--- SpringSecurity/src/main/java/com/security/controller/LoginController.java
+++ SpringSecurity/src/main/java/com/security/controller/LoginController.java
@@ -1,7 +1,11 @@
 package com.security.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Controller
